4cb53df687266432dbe22e38a627a8ccea39c9b6
Oswaldinho <oswalfut_96@hotmail.com>
2017-10-30 13:15:50 -0600

    router and material-ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import {MuiThemeProvider} from "material-ui";
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+injectTapEventPlugin();
+
+
+const WithRouter =()=>(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+);
+
+const Main = ()=>(
+    <MuiThemeProvider>
+        <WithRouter/>
+    </MuiThemeProvider>
+);
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
