68d64f80de677ce1a1821af4ece00344c4df42fa
Miguel Gonzalez <miglez90290995@gmail.com>
2017-10-30 13:38:13 -0600

    Creacion caja container component y ruta

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import {Switch, Route} from 'react-router-dom';
 import React from 'react';
 import InventarioPage from "./components/inventario/InventarioPage";
+import CajaComponent from "./components/caja/CajaComponent";
 
 
 const Routes = () => (
     <Switch>
         <Route path="/inventario" component={InventarioPage}/>
+        <Route path="/caja" component={CajaComponent}/>
     </Switch>
 );
 
