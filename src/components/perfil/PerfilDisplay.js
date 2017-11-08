a64516566c03f299806526fe687912874656ed6c
BlisS <contacto@fixter.org>
2017-11-08 14:37:24 -0600

    actions

--- src/components/perfil/PerfilDisplay.js
+++ src/components/perfil/PerfilDisplay.js
@@ -3,12 +3,14 @@
  */
 import React from 'react';
 
+const bliss = "http://blog.fixter.org/content/images/2017/11/bliss-3.jpg";
 
 export const PerfilDisplay = ({props}) => {
     return (
         <div>
-            <div className="perfil-cover">
-                <img src="" alt=""/>
+            <div className="profile-cover">
+                <button>Change Cover</button>
+                <img src={bliss} alt=""/>
             </div>
         </div>
     );
